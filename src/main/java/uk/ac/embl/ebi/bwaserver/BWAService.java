72a4886e384aec18ef14d8d89e2f151c8c966af0
asenf <asenf@asenf-virtual-machine>
2019-02-22 14:58:26 +0000

    Fix Test mode for larger FASTA files

--- src/main/java/uk/ac/embl/ebi/bwaserver/BWAService.java
+++ src/main/java/uk/ac/embl/ebi/bwaserver/BWAService.java
@@ -288,13 +288,15 @@ public class BWAService {
         for (int i=0; i<numIndexEntries; i++) {
             if (toArray[i] > (lenI-seqLen))
                 toArray[i] = (lenI-seqLen-1<0)?0:lenI-seqLen-1;
-            //if (toArray[i] < 100)
-            //    toArray[i] = 100;
             rafI.seek(toArray[i]);
             byte[] bTmp = new byte[seqLen];
             rafI.readFully(bTmp);
             testIndexEntries[i] = (new String(bTmp)).replaceAll("\n", "");
             testIndexEntries[i].replaceAll("[^ACTGN]", "");
+            if (testIndexEntries[i].contains(">") && testIndexEntries[i].indexOf(">") > 10)
+                testIndexEntries[i] = testIndexEntries[i].substring(0, testIndexEntries[i].indexOf(">"));
+            else if (testIndexEntries[i].contains(">"))
+                testIndexEntries[i] = testIndexEntries[i].substring(100);
             //System.out.println("Seq " + testIndexEntries[i]);
         }
         rafI.close();
